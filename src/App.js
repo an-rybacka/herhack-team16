3e608d380d5f274dccde89c7d8470072528eeb4c
an-rybacka <121604351+an-rybacka@users.noreply.github.com>
2023-11-24 13:25:15 +0100

    Prepare app skeleton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
+import { Button } from "primereact/button";
+
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="./images/plastic-pet2.jpg"
+          className="factory"
+          alt="factory"
+          height="400"
+        />
+        <p>hello you.</p>
+        <Home />
+        <Button label="Submit" />
       </header>
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+
+
+export default function Hero() {
+  return (
+    <div>
+      <p>this is home</p>
+      
+    </div>
+  );
+}
