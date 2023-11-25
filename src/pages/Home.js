dafd6cb10eabaf61a07194fb31a2337ff5c91fb0
an-rybacka <121604351+an-rybacka@users.noreply.github.com>
2023-11-25 15:29:26 +0100

    Adjust HomePage Buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,31 +17,28 @@ export default function Home({ onAddOffer, onBrowse, onTakeOffer }) {
         <div>
           <h3>What would you like to do today?</h3>
         </div>
-        <div className="space-y-2">
-          <Button
-            rounded
-            label="Create an offer" // I am a manufacturer that produces plastic waste. I want to connect with companies that recycle my waste so I can help the planet
-            severity="primary"
-            style={{ padding: "20px 40px" }}
-            className="m-2"
-            onClick={onAddOffer}
-          />
-          <Button
-            rounded
-            label="Take an offer" // I am recycling plastic materials. I want to make a request for materials and get matched with manufacturers.
-            severity="secondary"
-            style={{ padding: "20px 40px" }}
-            className="m-2"
-            onClick={onTakeOffer}
-          />
-          <Button
-            rounded
-            label="Browse available offers"
-            severity="secondary"
-            style={{ padding: "20px 40px" }}
-            className="m-2"
-            onClick={onBrowse}
-          />
+          <div className="buttons-homepage">
+            <Button
+              rounded
+              label="Create an offer" // I am a manufacturer that produces plastic waste. I want to connect with companies that recycle my waste so I can help the planet
+              severity="primary"
+              style={{ padding: "15px 30px", margin: "5px" }}
+              onClick={onAddOffer}
+            />
+            <Button
+              rounded
+              label="Take an offer" // I am recycling plastic materials. I want to make a request for materials and get matched with manufacturers.
+              severity="secondary"
+              style={{ padding: "15px 30px", margin: "5px" }}
+              onClick={onTakeOffer}
+            />
+            <Button
+              rounded
+              label="Browse available offers"
+              severity="secondary"
+              style={{ padding: "15px 30px", margin: "5px" , marginTop: "10px" }}
+              onClick={onBrowse}
+            />
         </div>
       </div>
       <div className="bottom"></div>
